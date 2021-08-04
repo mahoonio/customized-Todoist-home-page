694aecc0edce5ad354e036667b179e2ee3f3895c
Mahan <m.mahmodi1380@gmail.com>
2021-08-04 14:02:55 +0430

    js script beta, sorry there is a bug in adding elements that i will try to solve it before you check this repository

--- myScript.js
+++ myScript.js
@@ -8,3 +8,32 @@ function hideAddField()
    var x =  document.getElementById('AddField');
    x.style.display = "none";
 }
+
+
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".btn-add");
+
+
+
+
+function addToList(e){
+   const taskList = document.getElementById("pls");
+   console.log(addBtn);
+   ;
+   // var length = document.querySelectorAll("li").length;
+   // var id = "li"+length;
+   // const label = document.createElement('label');
+   // label.htmlFor = id;
+   const newLi = document.createElement('li');
+   const btn = document.createElement('button');
+   
+   
+   
+      newLi.appendChild(btn);
+      
+      // newLi.appendChild(btn);
+      // newLi.appendChild(label);
+      // taskInput.value = '';
+      taskList.appendChild(newLi);
+   
+}
