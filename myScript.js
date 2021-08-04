d25dbb15e582d69189bf3ca4084a5277a43ca688
Mahan <m.mahmodi1380@gmail.com>
2021-08-04 05:41:22 +0430

    task add field,listdesign and image

---
+++ myScript.js
@@ -0,0 +1,10 @@
+function showAddField()
+{
+   var x =  document.getElementById('AddField');
+   x.style.display = "block";
+}
+function hideAddField()
+{
+   var x =  document.getElementById('AddField');
+   x.style.display = "none";
+}
